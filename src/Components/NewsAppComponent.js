058e1b3a486232bba418a967fa7f3e87bb2fdfd3
Jack Portwood <Jack.Portwood14@gmail.com>
2020-11-10 13:08:40 +0000

    renders list

--- src/Components/NewsAppComponent.js
+++ src/Components/NewsAppComponent.js
@@ -1,34 +1,36 @@
 import React from 'react';
 import {getBitcoinArticles} from "../ApiRequests"
+import {Container, Header} from 'semantic-ui-react'
+import ArticleList from './ArticleList'
+
 
 class NewsAppComponent extends React.Component {
-
   state = {
-  articles: [],
-  apiError: "",
+    articles: [],
+    apiError: "",
   };
 
-
-async componentDidMount() {
-  try {
-    const response = await getBitcoinArticles();
-    this.setState({ articles: response.articles });
-  } catch (error) {
-    this.setState({ apiError: "Could not find any articles" });
+  async componentDidMount() {
+    try {
+      const response = await getBitcoinArticles();
+      this.setState({ articles: response.articles });
+    } catch (error) {
+      this.setState({ apiError: "Could not find any articles" });
+    }
   }
-}
 
- 
-
-render() {
-    
-    return(
-    
-    <div> Hello World! </div>
-        
-    )
-}
-
+  render() {
+    const { articles, apiError } = this.state;
+    return (
+      <Container>
+        <Header as="h2" style={{ textAlign: "center", margin: 20 }}>
+          Bitcoin articles
+        </Header>
+        {articles.length > 0 && <ArticleList articles={articles} />}
+        {apiError && <p>Could not fetch any articles. Please try again.</p>}
+      </Container>
+    );
+  }
 }
 
 export default NewsAppComponent;
