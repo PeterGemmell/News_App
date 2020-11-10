00d8f4d748a5f83372e10e7210fdfa96d0b84d51
Jack Portwood <Jack.Portwood14@gmail.com>
2020-11-10 15:35:58 +0000

    doesnt fetch

--- src/Components/NewsAppComponent.js
+++ src/Components/NewsAppComponent.js
@@ -1,31 +1,49 @@
 import React from 'react';
-import {getBitcoinArticles} from "../ApiRequests"
+import {getArticles} from "../ApiRequests"
 import {Container, Header} from 'semantic-ui-react'
 import ArticleList from './ArticleList'
+import SearchBar from "./SearchBar"
 
 
 class NewsAppComponent extends React.Component {
   state = {
     articles: [],
+    searchTopic:"",
+    totalResults:"",
+    loading: false,
     apiError: "",
   };
 
-  async componentDidMount() {
+  searchForTopic = async (topic) => {
     try {
-      const response = await getBitcoinArticles();
-      this.setState({ articles: response.articles });
+        this.setStat({loading:true});
+      const response = await getArticles(topic);
+      this.setState({ articles: response.articles, searchTopic: topic, totalResults: response.totalResults });
     } catch (error) {
       this.setState({ apiError: "Could not find any articles" });
     }
+    this.setState({loading: false})
   }
 
   render() {
-    const { articles, apiError } = this.state;
+    const { articles, searchTopic, totalResults,loading,apiError } = this.state;
     return (
       <Container>
         <Header as="h2" style={{ textAlign: "center", margin: 20 }}>
-          Bitcoin articles
+          Search for a topic
         </Header>
+        <SearchBar searchForTopic={this.searchForTopic} />
+        <p style={{ textAlign: "center" }}>
+          Powered by <a href="https://newsapi.org/">NewsAPI.org</a>
+        </p>
+        {loading && (
+          <p style={{ textAlign: "center" }}> Searching for articles...</p>
+        )}
+        {articles.length > 0 && (
+          <Header as="h4" style={{ textAlign: "center", margin: 20 }}>
+            Found {totalResults} articles on "{searchTopic}"
+          </Header>
+        )}
         {articles.length > 0 && <ArticleList articles={articles} />}
         {apiError && <p>Could not fetch any articles. Please try again.</p>}
       </Container>
