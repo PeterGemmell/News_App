cc9f0339ae624b52a6a57a101fe9c5c8a05e1b91
Jack Portwood <Jack.Portwood14@gmail.com>
2020-11-10 12:32:44 +0000

    data instate

--- src/ApiRequests.js
+++ src/ApiRequests.js
@@ -1,20 +1,15 @@
-import React from 'react';
 import {NEWS_API_KEY} from './Config'
 
+
+
 export const getBitcoinArticles = async () => {
+  const response = await fetch(
+    `https://newsapi.org/v2/everything?q=bitcoin&sortBy=publishedAt&apiKey=${NEWS_API_KEY}`
+  );
+  const json = await response.json();
+  return json;
 
-        const response = await fetch(
-          `https://newsapi.org/v2/everything?q=bitcoin&sortBy=publishedAt&apiKey=${NEWS_API_KEY}`
-        );
-        const json = await response.json();
-        return json;
-};
+  };
 
-async componentDidMount(){
-  try{
-  const response = await getBitcoinArticles();
-  this.setState({articles:response.articles})
-} catch (error){
-  this.setState({apiError:"Could not find articles"})
-}
-}
+
+
--- src/Components/NewsAppComponent.js
+++ src/Components/NewsAppComponent.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import {getBitcoinArticles} from "../ApiRequests"
 
-class NewsAppComponent extends React.component {
+class NewsAppComponent extends React.Component {
 
   state = {
   articles: [],
   apiError: "",
   };
+
+
+async componentDidMount() {
+  try {
+    const response = await getBitcoinArticles();
+    this.setState({ articles: response.articles });
+  } catch (error) {
+    this.setState({ apiError: "Could not find any articles" });
+  }
+}
+
+ 
+
 render() {
-
- return
-  <div> Hello World! </div>
-
+    
+    return(
+    
+    <div> Hello World! </div>
+        
+    )
 }
 
 }
